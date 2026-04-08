50daf2d151ce46362a4c3638e1c3124df51c7542
Martin Eliáš <martin.elias@oddin.gg>
2026-04-08 14:13:14 +0200

    fixup! Corwyn modifications

--- internal/feed/client.go
+++ internal/feed/client.go
@@ -94,7 +94,7 @@ func (c *Client) Open() error {
 		return err
 	}
 
-	vHost := fmt.Sprintf("%s", details.VirtualHost())
+	vHost := details.VirtualHost()
 	amqpURL := fmt.Sprintf(
 		"amqps://%s:%s@%s:%d",
 		*c.oddsFeedConfiguration.AccessToken(),
